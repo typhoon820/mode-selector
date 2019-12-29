af470519368e4c54350f2d570fcda8c6164aa0f3
Nikita Vasilyev <tpn820@gmail.com>
2019-12-29 13:03:28 +0300

    initial commit

---
+++ src/main/java/ru/heroes/modeselector/ModeSelectorApplication.java
@@ -0,0 +1,13 @@
+package ru.heroes.modeselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeSelectorApplication.class, args);
+	}
+
+}
