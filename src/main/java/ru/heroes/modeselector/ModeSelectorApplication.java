8d49a39fe417c0f26a676eb6bf0fd229faf608b2
typhoon820 <tpn820@gmail.com>
2022-12-20 13:42:39 +0300

    Trigger deploy

--- src/main/java/ru/heroes/modeselector/ModeSelectorApplication.java
+++ src/main/java/ru/heroes/modeselector/ModeSelectorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ModeSelectorApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModeSelectorApplication.class, args);
 	}
